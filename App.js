e11ccc79d81ac3cf2e71c0137f6f484c754efad6
hossein sharifi <hosseinsharifi@hotmail.com>
2018-03-12 19:35:49 +0330

    first commit

---
+++ App.js
@@ -0,0 +1,11 @@
+export App from './App';
+
+// class App extends Component {
+//   render() {
+//     return (
+//       <View>
+//         <Text> application </Text>
+//       </View>
+//     );
+//   }
+// }
---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+import { Header } from './components/common';
+import firebase from 'firebase';
+
+class App extends Component {
+  componentWillMount() {
+
+  }
+  render() {
+    return (
+      <View>
+        <Header headerText="Authentication" />
+        <Text> this application </Text>
+      </View>
+    );
+  }
+}
+
+export default App;
