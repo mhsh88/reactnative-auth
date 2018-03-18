6764f9f3841884de4ecb54883dbf313cc972fea8
hossein sharifi <hosseinsharifi@hotmail.com>
2018-03-18 12:34:30 +0330

    handling logging and logout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
 import firebase from 'firebase';
-import { Header } from './components/common';
+import { Header, Button, Spinner, CardSection } from './components/common';
 import LoginForm from './components/LoginForm';
 
 
 class App extends Component {
+  state = { loggedIn: null };
+
   componentWillMount() {
     firebase.initializeApp({
          apiKey: 'AIzaSyAWe1PhnLgNclXkGBebmepObhfddKKDcJQ',
@@ -15,12 +17,42 @@ class App extends Component {
          storageBucket: 'authentication-5ca18.appspot.com',
          messagingSenderId: '843206295963'
        });
+
+       firebase.auth().onAuthStateChanged((user) => {
+         if (user) {
+           this.setState({ loggedIn: true });
+         } else {
+           this.setState({ loggedIn: false });
+         }
+       });
   }
+
+renderContent() {
+  switch (this.state.loggedIn) {
+    case true:
+      return (
+        <CardSection>
+          <Button onPress={() => firebase.auth().signOut()}> Log Out </Button>
+        </CardSection>
+    );
+
+    case false:
+      return <LoginForm />;
+    default:
+      return (
+        <CardSection>
+          <Spinner size="large" />
+        </CardSection>
+      );
+  }
+}
+
+
   render() {
     return (
       <View>
         <Header headerText="Authentication" />
-        <LoginForm />
+        {this.renderContent()}
       </View>
     );
   }
